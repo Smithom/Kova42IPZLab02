6a4aa868a7807ec2140962dcd8c6ae77643b2635
sanja <sanjazzzkoval@gmail.com>
2019-03-25 22:43:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/koval24ipzlab02/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.koval24ipzlab02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         editText =(EditText)findViewById(R.id.editText);
+
+    }
+
+    public void  onClick(View view)
+    {
+      /* Intent intent = new Intent(this, NextActivity.class);
+        startActivity(intent);*/
+
+
+        Intent intent = new Intent(this, NextActivity.class);
+
+        String text = editText.getText().toString();
+
+        intent.putExtra("value", text);
+
+        Intent chooser = Intent.createChooser(intent, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
+
+
+    }
+}
